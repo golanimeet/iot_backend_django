5f9618d01a707d1e15a456cc8d0538737a2fd0dc
golanimeet <golanimeet7@gmail.com>
2025-02-04 15:43:16 +0530

    Initial commit - Uploading IoT project

---
+++ myapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import RegisterView, LoginView, DeviceListView, DeviceDetailView,ReportDataView, DashboardStatsView, toggle_device_status,PasswordResetRequestView,PasswordResetConfirmView
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('devices/', DeviceListView.as_view(), name='device-list'),
+    path('devices/<int:device_id>/', DeviceDetailView.as_view(), name='device-detail'),
+    path('toggle-device-status/<int:device_id>/', toggle_device_status, name='toggle_device_status'),
+    path('report-data/', ReportDataView.as_view(), name='report-data'),
+    path('dashboard-stats/', DashboardStatsView.as_view(), name='dashboard-stats'),
+
+
+    path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
+    path('password-reset-confirm/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+
+]
